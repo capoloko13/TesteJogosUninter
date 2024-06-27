956e07a87230baec4926433322f2c2b20b2e2cf1
RenanCapolupo <lifelupo@hotmail.com>
2024-06-26 21:37:27 -0300

    Signed-off-by: RenanCapolupo <lifelupo@hotmail.com>

---
+++ Model/game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class game:
+    def __init__(self):
+        self.screen = None
+
+    def run(self, ):
+        pass
+
---
+++ Model/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class level:
+    def __init__(self):
+        self.screen = None
+        self.name = None
+
+    def run(self, ):
+        pass
+
